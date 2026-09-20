5c7f68c2167e61005ab396e43a1fe70f92724505
Debashis <143251728+Debashiskhan2904@users.noreply.github.com>
2026-09-20 16:02:37 +0530

    Delete src directory

--- src/components/PageWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const PageWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="page active" style={{ display: 'block' }}>
-      {children}
-    </div>
-  );
-};
